bb2a8df6cd40374c7f48ad7183810d5383f91932
Olena Hryk <olena.andrushchakevych@gmail.com>
2024-05-13 21:52:49 +0300

    Changed icon_path from MUI to svg

--- src/consts/categories.js
+++ src/consts/categories.js
@@ -2,77 +2,77 @@ const categories = {
   LANGUAGES: {
     _id: '64884f21fdc2d1a130c24ac0',
     name: 'Languages',
-    appearance: { icon_path: 'LanguageRounded', color: '#7BB362' }
+    appearance: { icon_path: 'languages.svg', color: '#7BB362' }
   },
   MATHEMATICS: {
     _id: '64884f33fdc2d1a130c24ac2',
     name: 'Mathematics',
-    appearance: { icon_path: 'TagRounded', color: '#F2BE22' }
+    appearance: { icon_path: 'mathematics.svg', color: '#F2BE22' }
   },
   COMPUTER_SCIENCE: {
     _id: '64884fedfdc2d1a130c24ade',
     name: 'Computer science',
-    appearance: { icon_path: 'MonitorRounded', color: '#5A8088' }
+    appearance: { icon_path: 'computer_science.svg', color: '#5A8088' }
   },
   MUSIC: {
     _id: '64884f4dfdc2d1a130c24ac6',
     name: 'Music',
-    appearance: { icon_path: 'MusicNoteRounded', color: '#B35969' }
+    appearance: { icon_path: 'music.svg', color: '#B35969' }
   },
   DESIGN: {
     _id: '64884f59fdc2d1a130c24ac8',
     name: 'Design',
-    appearance: { icon_path: 'DesignServicesRounded', color: '#00A7A7' }
+    appearance: { icon_path: 'design.svg', color: '#00A7A7' }
   },
   HISTORY: {
     _id: '64884f66fdc2d1a130c24aca',
     name: 'History',
-    appearance: { icon_path: 'LanguageRounded', color: '#EF4257' }
+    appearance: { icon_path: 'history.svg', color: '#EF4257' }
   },
   BIOLOGY: {
     _id: '64884f70fdc2d1a130c24acc',
     name: 'Biology',
-    appearance: { icon_path: 'BiotechRounded', color: '#5A8088' }
+    appearance: { icon_path: 'biology.svg', color: '#5A8088' }
   },
   PAINTING: {
     _id: '64884f7afdc2d1a130c24ace',
     name: 'Painting',
-    appearance: { icon_path: 'PaletteRounded', color: '#79B260' }
+    appearance: { icon_path: 'painting.svg', color: '#79B260' }
   },
   FINANCES: {
     _id: '64884f83fdc2d1a130c24ad0',
     name: 'Finances',
-    appearance: { icon_path: 'AccountBalanceRounded', color: '#F1BC17' }
+    appearance: { icon_path: 'finances.svg', color: '#F1BC17' }
   },
   MARKETING: {
     _id: '64884f8efdc2d1a130c24ad2',
     name: 'Marketing',
-    appearance: { icon_path: 'LocalGroceryStoreRounded', color: '#1784F1' }
+    appearance: { icon_path: 'marketing.svg', color: '#1784F1' }
   },
   AUDIT: {
     _id: '64884f98fdc2d1a130c24ad4',
     name: 'Audit',
-    appearance: { icon_path: 'LegendToggleRounded', color: '#EF4F61' }
+    appearance: { icon_path: 'audit.svg', color: '#EF4F61' }
   },
   CHEMISTRY: {
     _id: '64884fa2fdc2d1a130c24ad6',
     name: 'Chemistry',
-    appearance: { icon_path: 'ScienceRounded', color: '#B35969' }
+    appearance: { icon_path: 'chemistry.svg', color: '#B35969' }
   },
   ASTRONOMY: {
     _id: '64884fb0fdc2d1a130c24ad8',
     name: 'Astronomy',
-    appearance: { icon_path: 'StarRateRounded', color: '#00A7A7' }
+    appearance: { icon_path: 'astronomy.svg', color: '#00A7A7' }
   },
   PHYSICS: {
     _id: '64884fc3fdc2d1a130c24ada',
     name: 'Physics',
-    appearance: { icon_path: 'ElectricBoltRounded', color: '#F18D17' }
+    appearance: { icon_path: 'physics.svg', color: '#F18D17' }
   },
   PSYCHOLOGY: {
     _id: '64884fe3fdc2d1a130c24adc',
     name: 'Psychology',
-    appearance: { icon_path: 'PsychologyRounded', color: '#CD4EAC' }
+    appearance: { icon_path: 'psychology.svg', color: '#CD4EAC' }
   }
 }
 
